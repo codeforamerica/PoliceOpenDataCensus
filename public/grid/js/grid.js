6f2b452b48edc35179888c88eff9f97b50c64c4c
Chris Reade <creade@codeforamerica.org>
2015-05-20 18:32:23 -0400

    fixing tool tip copy

--- public/grid/js/grid.js
+++ public/grid/js/grid.js
@@ -5,7 +5,32 @@
          parseNumbers: true
      });
  });
+var captions = {
+"free":{
+    "Yes":"The data is available online",
+"No":"The data is not available online",
+"Unsure":"It is unclear whether the data is available online"},
 
+"machine":{"Yes":"It's machine readable",
+"No":"It is not machine readable",
+"Unsure":"It is unclear whether the data is machine readable"},
+
+"context":{"Yes":"Context is provided",
+"No":"Context is not provided ",
+"Unsure":"It is unclear whether the contect is provided"},
+
+"bulk":{"Yes":"Data can be downloaded in bulk ",
+"No":"Data cannot be downloaded in bulk  ",
+"Unsure":"It is unclear whether the data can be downloaded in bulk"},
+
+"fresh":{"Yes":"Data is up-to-date",
+"No":"Data is not up-to-date ",
+"Unsure":"It is unclear whether the data is up-to-date"},
+
+"incident":{"Yes":"Data shows individual incidents ",
+"No":"Data does not show individual incidents",
+"Unsure":"It is unclear whether the data shows individual incidents"}
+}
  var rawData = [];
 
  function showInfo(data, tabletop) {
@@ -38,18 +63,32 @@
                      return dataset["Type of Data"] === type;
                  })
                  if (foundDataset) {
-                     row["datasets"].push({
+                    var gridData = {
                          free: foundDataset["Data is freely available online"],
+
                          machine: foundDataset["Data is machine readable"],
+
                          context: foundDataset["Context is provided"],
+
                          bulk: foundDataset["Available in bulk"],
+
                          fresh: foundDataset["Up-to-date"],
+
                          incident: foundDataset["Incident-level data"],
+
                          datasetHref: URI().filename("datasets.html").search({
                              "department": row["department"],
                              "datatype": foundDataset["Type of Data"]
-                         }).toString()
-                     });
+                         })
+                     }
+
+                     gridData.freeCaption = captions.free[gridData.free];
+                     gridData.machineCaption = captions.machine[gridData.machine];
+                     gridData.contextCaption = captions.context[gridData.context];
+                     gridData.bulkCaption = captions.bulk[gridData.bulk];
+                     gridData.freshCaption = captions.fresh[gridData.fresh];
+                     gridData.incidentCaption = captions.incident[gridData.incident];
+                     row["datasets"].push(gridData).toString()
                  } else {
                      row["datasets"].push({
                          free: "DNE",
@@ -72,6 +111,9 @@
 
      $('[data-toggle="tooltip"]').tooltip()
  }
+
+
+ 
 
  function setupDatatypes(allTypes) {
      var datatypes = _.chain(allTypes).map(function(type) {
