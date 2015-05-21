f4486824aed47f093ddf9b1fb1a7253875eca3f2
Chris Reade <creade@codeforamerica.org>
2015-05-20 21:07:33 -0400

    formatting

--- public/grid/js/grid.js
+++ public/grid/js/grid.js
@@ -5,32 +5,45 @@
          parseNumbers: true
      });
  });
-var captions = {
-"free":{
-    "Yes":"The data is available online",
-"No":"The data is not available online",
-"Unsure":"It is unclear whether the data is available online"},
 
-"machine":{"Yes":"It's machine readable",
-"No":"It is not machine readable",
-"Unsure":"It is unclear whether the data is machine readable"},
+ var captions = {
+     "free": {
+         "Yes": "The data is available online",
+         "No": "The data is not available online",
+         "Unsure": "It is unclear whether the data is available online"
+     },
 
-"context":{"Yes":"Context is provided",
-"No":"Context is not provided ",
-"Unsure":"It is unclear whether the contect is provided"},
+     "machine": {
+         "Yes": "It's machine readable",
+         "No": "It is not machine readable",
+         "Unsure": "It is unclear whether the data is machine readable"
+     },
 
-"bulk":{"Yes":"Data can be downloaded in bulk ",
-"No":"Data cannot be downloaded in bulk  ",
-"Unsure":"It is unclear whether the data can be downloaded in bulk"},
+     "context": {
+         "Yes": "Context is provided",
+         "No": "Context is not provided ",
+         "Unsure": "It is unclear whether the contect is provided"
+     },
 
-"fresh":{"Yes":"Data is up-to-date",
-"No":"Data is not up-to-date ",
-"Unsure":"It is unclear whether the data is up-to-date"},
+     "bulk": {
+         "Yes": "Data can be downloaded in bulk ",
+         "No": "Data cannot be downloaded in bulk  ",
+         "Unsure": "It is unclear whether the data can be downloaded in bulk"
+     },
 
-"incident":{"Yes":"Data shows individual incidents ",
-"No":"Data does not show individual incidents",
-"Unsure":"It is unclear whether the data shows individual incidents"}
-}
+     "fresh": {
+         "Yes": "Data is up-to-date",
+         "No": "Data is not up-to-date ",
+         "Unsure": "It is unclear whether the data is up-to-date"
+     },
+
+     "incident": {
+         "Yes": "Data shows individual incidents ",
+         "No": "Data does not show individual incidents",
+         "Unsure": "It is unclear whether the data shows individual incidents"
+     }
+ };
+
  var rawData = [];
 
  function showInfo(data, tabletop) {
@@ -63,7 +76,7 @@ var captions = {
                      return dataset["Type of Data"] === type;
                  })
                  if (foundDataset) {
-                    var gridData = {
+                     var gridData = {
                          free: foundDataset["Data is freely available online"],
 
                          machine: foundDataset["Data is machine readable"],
@@ -113,7 +126,7 @@ var captions = {
  }
 
 
- 
+
 
  function setupDatatypes(allTypes) {
      var datatypes = _.chain(allTypes).map(function(type) {
