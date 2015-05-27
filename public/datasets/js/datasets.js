74ec886134daee4437d613c447817324d17fef3e
TiffanyAndrews <tiffanyjandrews@gmail.com>
2015-05-27 16:05:25 -0400

    add data breakdown colors to cards page

--- public/datasets/js/datasets.js
+++ public/datasets/js/datasets.js
@@ -6,6 +6,44 @@
      });
  });
 
+ var captions = {
+     "free": {
+         "Yes": "The data is available online",
+         "No": "The data is not available online",
+         "Unsure": "It is unclear whether the data is available online"
+     },
+
+     "machine": {
+         "Yes": "It's machine readable",
+         "No": "It is not machine readable",
+         "Unsure": "It is unclear whether the data is machine readable"
+     },
+
+     "context": {
+         "Yes": "Context is provided",
+         "No": "Context is not provided ",
+         "Unsure": "It is unclear whether the contect is provided"
+     },
+
+     "bulk": {
+         "Yes": "Data can be downloaded in bulk ",
+         "No": "Data cannot be downloaded in bulk  ",
+         "Unsure": "It is unclear whether the data can be downloaded in bulk"
+     },
+
+     "fresh": {
+         "Yes": "Data is up-to-date",
+         "No": "Data is not up-to-date ",
+         "Unsure": "It is unclear whether the data is up-to-date"
+     },
+
+     "incident": {
+         "Yes": "Data shows individual incidents ",
+         "No": "Data does not show individual incidents",
+         "Unsure": "It is unclear whether the data shows individual incidents"
+     }
+ };
+
  var allRows = [];
 
  function showInfo(data, tabletop) {
@@ -31,16 +69,35 @@
 
      _.chain(rows)
          .filter(function(row) {
-             return _.all(filters, function(filter) {
-                 return filter(row);
-             });
-         }).map(function(row) {
+                 return _.all(filters, function(filter) {
+                     return filter(row);
+                 });
+        })
+         .map(function(row) {
+
+            row.free = row["Data is freely available online"];
+            row.machine = row["Data is machine readable"];
+            row.context = row["Context is provided"];
+            row.bulk = row["Available in bulk"];
+            row.fresh = row["Up-to-date"];
+            row.incident = row["Incident-level data"];
+                 
+
+             row.freeCaption = captions.free[row.free];
+             row.machineCaption = captions.machine[row.machine];
+             row.contextCaption = captions.context[row.context];
+             row.bulkCaption = captions.bulk[row.bulk];
+             row.freshCaption = captions.fresh[row.fresh];
+             row.incidentCaption = captions.incident[row.incident];
 
              var html = template(row);
-             $("#cards").append(html);
-             $('[data-toggle="tooltip"]').tooltip();  
-         });
+            $("#cards").append(html);
+            $('[data-toggle="tooltip"]').tooltip();  
+        });
  }
+
+
+
 
  function buildDepartmentFilter(department) {
      if (!department) {
