56e15203cb283b1eabfea5d666add1ca34907271
Rebecca Williams <rebecca@rebeccawilliams.us>
2015-09-29 14:40:53 -0400

    recommendations!

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var underscore = require('underscore');
 var merge = require('merge-stream');
 var install = require("gulp-install");
 
-var modules = ["datasets", "grid", "datatype", "recomendations"];
+var modules = ["datasets", "grid", "datatype", "recommendations"];
 
 gulp.task('default', ["bower", "clean", "buildDev"]);
 
